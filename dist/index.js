bf741365e5482185c9c78b611aa0a37baaff61aa
erhanyil <erhan_yil@windowslive.com>
2023-06-14 14:56:13 +0300

    added api_url input

--- dist/index.js
+++ dist/index.js
@@ -4134,7 +4134,7 @@ async function run() {
         await exec.exec(`docker pull cyprox/scan-engine:latest -q`);
         try {
             core.setSecret(core.getInput('api_key'))
-            await exec.exec(`docker run -v ${workspace}:/:rw cyprox/scan-engine --api-key=${core.getInput('api_key')} ${core.getInput('api_url') ? ('--api-url='+core.getInput('api_url')) : ''} --scan-id=${core.getInput('scan_id')} --tag-name=${core.getInput('tag_name') ? core.getInput('tag_name') : ('github-action-'+currentRunnerID)} --o=true`);
+            await exec.exec(`docker run -v ${workspace}:/output:rw cyprox/scan-engine --api-key=${core.getInput('api_key')} ${core.getInput('api_url') ? ('--api-url='+core.getInput('api_url')) : ''} --scan-id=${core.getInput('scan_id')} --tag-name=${core.getInput('tag_name') ? core.getInput('tag_name') : ('github-action-'+currentRunnerID)} --o=true`);
         } catch (err) {
             core.setFailed(err.message);
         }
